b9cc7029ee828fe633d7cf88aa53e888bafdbaec
jdietrich11 <jbdietrich2@gmail.com>
2022-02-11 02:39:19 -0700

    started google auth component

---
+++ src/components/GoogleAuth.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
+          clientId:
+            '601917407860-af0lfhr27acp3pirnn42dvfaib7see1d.apps.googleusercontent.com',
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  toggleSignIn = () => {
+    return this.auth.signIn();
+  };
+  toggleSignOut = () => {
+    return this.auth.signOut();
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.state.isSignedIn) {
+      return (
+        <button onClick={this.toggleSignOut} className='ui red google button'>
+          <i className='google icon' />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={this.toggleSignIn} className='ui red google button'>
+          <i className='google icon' />
+          Sign in with Google
+        </button>
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
